059948bf68f40c2d0bf8de7024331fa69376d4f6
tz3tzis <mtzetzis@hotmail.com>
2018-09-25 20:09:54 +0300

    logout heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	root "pages#home"
 
   # log in page with form:
-	get '/login'     => 'sessions#new'
+	get '/login'     => 'sessions#new' 
 	
 	# create (post) action for when log in form is submitted:
 	post '/login'    => 'sessions#create'
@@ -11,14 +11,18 @@ Rails.application.routes.draw do
 	# delete action to log out:
 	delete '/logout' => 'sessions#destroy' 
 
-	#get '/admins/:id/edit' => 'admins#edit'
 
+	#get admin distinct program
  	get '/admins/:id' => 'admins#show' , :as => 'show'
+
+ 	#get upadate password form
  	get '/admins/:id/edit' => 'admins#edit' , :as => 'edit_admin'
 
+ 	#post changes of the password
 	post '/admins/:id' => 'admins#update' , :as => 'update'
 
 	resources :games 
 
   resources :admins , :except => [:new,:index]
+  
 end
