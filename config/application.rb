cdd293fec0caefb3eb374c1728a16156fa73b657
tz3tzis <mtzetzis@hotmail.com>
2018-10-02 18:01:27 +0300

    heroku errors resolution

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,8 @@
+require_relative 'boot'
+
+require 'rails/all'
+require 'carrierwave'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
