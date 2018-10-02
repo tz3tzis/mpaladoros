789fffd17e3392ec551a505c9d02be39f59b4288
tz3tzis <mtzetzis@hotmail.com>
2018-10-02 14:28:18 +0300

    fb login v16

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,9 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do 
 	provider :facebook, '213485926125142', 'c50dff834562bf16fa9506ef8c6369fd' ,
 	scope:  'email',
-  display: 'popup'
+  display: 'popup',
+  client_options: {
+      site: 'https://graph.facebook.com/v3.0',  # this is the example API version
+      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+    }
 end
