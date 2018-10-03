67799a84ef2fd7e4a6a345fee0f21ae56fd3ac64
tz3tzis <mtzetzis@hotmail.com>
2018-10-03 16:44:19 +0300

    fb login redirect to gassssssmesss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,13 +18,4 @@ class ApplicationController < ActionController::Base
 	end
 
 
-	def after_sign_in_path_for(resource)
-     stored_location_for(resource) ||
-    if resource.is_a?(User)
-      games_url
-    else
-      super
-    end
-  end
-
 end
