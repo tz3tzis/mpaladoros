c164962960c5e2695092b8c51e3b5a3408539758
tz3tzis <mtzetzis@hotmail.com>
2018-10-03 17:48:25 +0300

    still logout problems

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -6,7 +6,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      redirect_to games_path
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
