39fe9771a6b2efd3a3b36b004706e284b498351d
tz3tzis <mtzetzis@hotmail.com>
2018-10-08 20:38:57 +0300

    mia malakitsa

--- app/controllers/stadia_controller.rb
+++ app/controllers/stadia_controller.rb
@@ -11,6 +11,6 @@ class StadiaController < ApplicationController
 	 def admin_params
     # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
     # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:stadium).permit(:id, :stadium_name, :max_players, :admin_id, :game_id)
+    params.require(:stadium).permit(:id, :stadium_name, :admin_id)
 	
 end
