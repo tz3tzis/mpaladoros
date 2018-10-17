d57b78fac8f9f78b31e0bbf1f241687d8db1b5b7
tz3tzis <mtzetzis@hotmail.com>
2018-10-17 18:44:18 +0300

    errorzz done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
- 	before_filter :set_current_user
+ 	before_action :set_current_user
 
 	def set_current_user
 	  User.current_user = current_user
