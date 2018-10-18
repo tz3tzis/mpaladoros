0f578baca1e38074cc28cb518cdf7e381d11b10b
tz3tzis <mtzetzis@hotmail.com>
2018-10-18 12:31:19 +0300

    take picture from fb vol 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
 	    user.name = auth.info.name
-	    user.image = http://graph.facebook.com/#{auth.uid}/picture?type=normal
+	    user.image = URI.parse(auth.info.image) if auth.info.image?
       user.save!
   	end  	
   end
