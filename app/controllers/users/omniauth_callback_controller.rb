fe69e5f44f5b4922be9c6c633a404c2c377ad8e6
tz3tzis <mtzetzis@hotmail.com>
2018-10-21 14:15:02 +0300

    fb login problems

--- app/controllers/users/omniauth_callback_controller.rb
+++ app/controllers/users/omniauth_callback_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in_and_redirect @user
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
