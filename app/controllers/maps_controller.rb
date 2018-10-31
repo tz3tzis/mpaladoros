b438dd2033b7c4fc4f902b710550685203204166
tz3tzis <mtzetzis@hotmail.com>
2018-10-31 20:37:10 +0200

    map markers etc

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,8 +3,6 @@ class MapsController < ApplicationController
 	def index
 		@game = Game.find(params[:game_id])
 		stadium = Stadium.find(@game.stadium_id)
-		lati  =  stadium.latitude 
-		longi =  stadium.longitude
 	end
 
 
