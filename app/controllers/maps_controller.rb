6c92e6af3d1b7bda01d45730b3baffceb2cc85e5
tz3tzis <mtzetzis@hotmail.com>
2018-10-31 20:11:04 +0200

    added link to location at games

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,12 +2,14 @@ class MapsController < ApplicationController
 
 	def index
 		@game = Game.find(params[:game_id])
+		stadium = Stadium.find(@game.stadium_id)
+		lati  =  stadium.latitude 
+		longi =  stadium.longitude
 	end
 
 
 	def create
 		
-
 	end
 
 end
