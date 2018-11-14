fa3244a34fa010077c9f056c499dc02eb8f1a3da
tz3tzis <mtzetzis@hotmail.com>
2018-11-14 14:13:05 +0200

    bot bot bot

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,32 @@ include Facebook::Messenger
 #Facebook::Messenger::Subscriptions.subscribe(access_token:ENV["ACCESS_TOKEN"])  
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  message.reply(text: 'Καλησπέρα παιχταρά μου!')
+  message.reply(
+	  attachment: {
+	    type: 'template',
+	    payload: {
+	      template_type: 'button',
+	      text: 'Σου αρέσει η εφαρμογη ?',
+	      buttons: [
+	        { type: 'postback', title: 'Ναι', payload: 'yes' },
+	        { type: 'postback', title: 'Οχι', payload: 'no' }
+	      ]
+	    }
+	  }
+	)
+end
+
+Bot.on :postback do |postback|
+	postback.sender
+	postback.recipient
+	postback.sent_at
+	postback.payload
+
+	if postback.payload == "yes"
+		puts "Τότε #{postback.recepient} μπες εδω(www.mpaladoros.gr) και ξεκίνα το παιχνίδι!"
+	elsif postback.payload == 'no'
+		puts "ΑΝΤΕ ΓΕΙΑ!"
+	end
+
 end
