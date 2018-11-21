7da986e9e51e1f100ac7df5bab15fe2ca5185bb6
tz3tzis <mtzetzis@hotmail.com>
2018-11-21 13:37:42 +0200

    one commit a day keep the doctor away

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   geocoded_by :ip_address
   after_validation :geocode 
 
-  
-
   def self.new_with_session(params,session)
   	super.tap do |user|
   		if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
