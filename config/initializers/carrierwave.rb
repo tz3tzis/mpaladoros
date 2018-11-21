44c1dc69367095b693e34859feecea98aa9f70c9
tz3tzis <mtzetzis@hotmail.com>
2018-11-21 15:20:46 +0200

    one commit a day keep the doctor away

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ if Rails.env.production?
       provider:              'AWS',                        # required
       aws_access_key_id:     ENV["S3_ACCESS_KEY_ID"],      # required unless using use_iam_profile
       aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],  # required unless using use_iam_profile
-     
+      region: 'eu-central-1'
     }
-    config.fog_directory  =  ENV["S3_BUCKET"]               # required
+    config.fog_directory  =  ENV["S3_BUCKET"]              # required
   end
 end
