cd35e3787efbfd46983581ec4e87e15407b1db38
tz3tzis <mtzetzis@hotmail.com>
2018-11-29 14:19:06 +0200

    some changesclear

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, User 
-    return unless user.present?
-    can :manage, User, id: user.id
+  	if user.is_a?(User)
+	    can :read, User 
+	    return unless user.present?
+	    can :manage, User, id: user.id
+	  elsif user.is_a?(Admin)
+	  	can [:read, :update], Admin, id: admin.id
+	  end
+	  	
   end
 
  # def initialize(admin)
