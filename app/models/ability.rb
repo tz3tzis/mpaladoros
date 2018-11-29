1213587c043c8c9ef3f70471c2b07b2bcf4f61b5
tz3tzis <mtzetzis@hotmail.com>
2018-11-29 13:02:16 +0200

    some changesclear

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,5 @@
-class Ability
+
+class UserAbility
   include CanCan::Ability
 
   def initialize(user)
@@ -6,10 +7,24 @@ class Ability
     return unless user.present?
     can :manage, User, id: user.id
   end
+end
+
+class AdminAbility
+  include CanCan::Ability
 
   def initialize(admin)
-  	can :read, Admin, id: admin.id
-  	can :manage, Admin, id: admin.id
+    can [:read, :update], Admin, id: admin.id
   end
-
 end
+
+class ApplicationController < ActionController::Base
+  # overriding CanCan::ControllerAdditions
+  def current_ability
+    if current_account.kind_of?(AdminUser)
+      @current_ability ||= AdminAbility.new(current_account)
+    else
+      @current_ability ||= UserAbility.new(current_account)
+    end
+  end
+end
+
