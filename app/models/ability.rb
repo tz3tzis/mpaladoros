248ab4d921533d8444a8a4d0289fcefeecd484a1
tz3tzis <mtzetzis@hotmail.com>
2018-12-03 12:40:48 +0200

    ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,11 +3,11 @@ class Ability
 
   def initialize(userOrAdmin)
 
-  	if userOrAdmin.user?
+  	if userOrAdmin.is_a? User
 	    can :read, User 
 	    return unless user.present?
 	    can :manage, User, id: user.id
-	  elsif userOrAdmin.admin? 
+	  elsif userOrAdmin.is_a?  Admin
 	  	can [:update, :read] , Admin, id: admin.id
 	  end
   end
