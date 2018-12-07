52d92e4d0bb86af37d1fc7812a2c8991afe7e114
tz3tzis <mtzetzis@hotmail.com>
2018-12-07 15:44:58 +0200

    Web push notifications

--- vendor/bundle/ruby/2.4.0/gems/hashie-3.5.7/spec/integration/omniauth/app.rb
+++ vendor/bundle/ruby/2.4.0/gems/hashie-3.5.7/spec/integration/omniauth/app.rb
@@ -9,3 +9,19 @@ class MyApplication < Sinatra::Base
     'Hello World'
   end
 end
+
+
+post '/push' do
+  Webpush.payload_send(
+    message: params[:message]
+    endpoint: params[:subscription][:endpoint],
+    p256dh: params[:subscription][:keys][:p256dh],
+    auth: params[:subscription][:keys][:auth],
+    ttl: 24 * 60 * 60,
+    vapid: {
+      subject: 'mailto:sender@example.com',
+      public_key: ENV['VAPID_PUBLIC_KEY'],
+      private_key: ENV['VAPID_PRIVATE_KEY']
+    }
+  )
+end
