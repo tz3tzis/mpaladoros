426b1a2639f7e1cadf39b169d9626eef3c706e13
tz3tzis <mtzetzis@hotmail.com>
2018-12-08 19:11:32 +0200

    push notifications

--- app/assets/javascripts/serviceworker.js
+++ app/assets/javascripts/serviceworker.js
@@ -4,7 +4,7 @@ self.addEventListener("push", (event) => {
   var title = (event.data && event.data.text().split("-")[0]) || "Yay a message";
   var body; body = event.data.text().split("-")[1];
   var tag = "push-simple-demo-notification-tag";
-  var icon = '/assets/logo.png';
+  var icon = '/assets/my-push-logo-192x192.png';
 
   event.waitUntil(
     self.registration.showNotification(title, {
--- app/controllers/notify_controller.rb
+++ app/controllers/notify_controller.rb
@@ -5,16 +5,17 @@ class NotifyController < ApplicationController
     if user.notification_data_id.present?
       @notification_data = NotificationData.find(user.notification_data_id)
       Webpush.payload_send(endpoint: @notification_data.endpoint,
-                           message: @message,
-                           p256dh: @notification_data.p256dh_key,
-                           auth: @notification_data.auth_key,
-                           ttl: 24 * 60 * 60,
-                           vapid: {
-                               subject: 'mailto:admin@commercialview.com.au',
-                               public_key: ENV['VAPID_PUBLIC_KEY'],
-                               private_key: ENV['VAPID_PRIVATE_KEY']
-                           }
+          message: @message,
+          p256dh: @notification_data.p256dh_key,
+          auth: @notification_data.auth_key,
+          tl: 24 * 60 * 60,
+          vapid: {
+              subject: 'mailto:admin@commercialview.com.au',
+              public_key: ENV['VAPID_PUBLIC_KEY'],
+              private_key: ENV['VAPID_PRIVATE_KEY']
+          }
       )
     end
   end
+
 end
