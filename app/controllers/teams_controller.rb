238a0741a2ff42ce9e8a394a4ce84ce0a2b51683
tz3tzis <mtzetzis@hotmail.com>
2018-12-08 20:26:38 +0200

    push notifications

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -42,6 +42,30 @@ class TeamsController < ApplicationController
 	end
 
 
+	def push
+    user = User.find(params[:id])
+    @message = get_message()
+    if user.notification_data_id.present?
+      @notification_data = NotificationData.find(user.notification_data_id)
+      Webpush.payload_send(endpoint: @notification_data.endpoint,
+          message: @message,
+          p256dh: @notification_data.p256dh_key,
+          auth: @notification_data.auth_key,
+          tl: 24 * 60 * 60,
+          vapid: {
+              subject: 'mailto:admin@commercialview.com.au',
+              public_key: ENV['VAPID_PUBLIC_KEY'],
+              private_key: ENV['VAPID_PRIVATE_KEY']
+          }
+      )
+    end
+
+    lash[:notice] = "Θα ειδοποιηθείς στον browser για την συνέχεια της διαδικασίας"
+    redirect_to games_url
+
+  end
+
+
  def team_params
  	params.
  		require(:team).
