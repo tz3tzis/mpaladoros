d98673d078caa695177dafd9f4e05d51cd0c85e3
tz3tzis <mtzetzis@hotmail.com>
2018-12-09 16:53:53 +0200

    push notifications

--- app/controllers/push_notifications_controller.rb
+++ app/controllers/push_notifications_controller.rb
@@ -1,8 +1,9 @@
 class PushNotificationsController < ApplicationController
 	
 	def create
+		post '/push' do
 		  Webpush.payload_send(
-		    message: params[:message]
+		    message: params[:message],
 		    endpoint: params[:subscription][:endpoint],
 		    p256dh: params[:subscription][:keys][:p256dh],
 		    auth: params[:subscription][:keys][:auth],
@@ -13,5 +14,6 @@ class PushNotificationsController < ApplicationController
 		      private_key: ENV['VAPID_PRIVATE_KEY']
 		    }
 		  )
+		end
 	end
 end
