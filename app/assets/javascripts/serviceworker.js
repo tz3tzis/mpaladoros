1a6f203b6f59cab3eb695a73f7b1b1ca57adf65e
tz3tzis <mtzetzis@hotmail.com>
2018-12-09 14:56:52 +0200

    push notifications

--- app/assets/javascripts/serviceworker.js
+++ app/assets/javascripts/serviceworker.js
@@ -1,15 +1,16 @@
 // The serviceworker context can respond to 'push' events and trigger
 // notifications on the registration property
-function onPush(event) {
-  var title = (event.data && event.data.text()) || "Yay a message";
+self.addEventListener("push", function(event) {
+  var title = (event.data && event.data.text().split("-")[0]) || "Message recieved";
+  var body; body = event.data.text().split("-")[1];
+  var tag = "push-simple-demo-notification-tag";
+  var icon = 'logo.png';
 
   event.waitUntil(
-    self.registration.showNotification(title, {
-      body: "We have received a push message",
-      icon: "/assets/path/to/icon.png",
-      tag:  "push-simple-demo-notification-tag"
-    })
-  )
-}
-
-self.addEventListener("push", onPush);
+      self.registration.showNotification(title, {
+          body: body,
+          icon: icon,
+          tag: tag
+      })
+  );
+});
