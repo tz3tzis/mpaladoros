5ccd5b6a8e44094839bdfcf2df82d31f8acb95e0
tz3tzis <mtzetzis@hotmail.com>
2018-12-14 17:37:16 +0200

    test script

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -10,7 +10,6 @@ module GamesHelper
 			1.upto(count) do
 				threads << Thread.new do 
 					response = open('https://mpaladoros-app.herokuapp.com/users/auth/facebook').read
-					sleep(3)
 				end
 			end
 			threads.map(&:join)
