5116ad5760c6117246f082bc85c4fa33e65a30e7
tz3tzis <mtzetzis@hotmail.com>
2018-12-14 17:41:19 +0200

    test script

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,7 +12,7 @@ class GamesController < ApplicationController
 		@admin=Admin.all.find_by(@game.admin_id) if !current_game.nil?
 
 		#===========test sumultaneous users=========
-		@time = get_test(100) 
+		@time = get_test(1000) 
 		puts @time 
 		#==========================================
 
