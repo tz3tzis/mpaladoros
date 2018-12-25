a5e80b86161f5903ab61d6877fef37b5e46669d3
tz3tzis <mtzetzis@hotmail.com>
2018-12-25 18:03:47 +0100

    merry christmas

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,7 @@ class User < ApplicationRecord
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
 	    user.name = auth.info.name
+      user.avatar = largeimage
       user.save!
   	end  	
   end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,4 @@
+
 if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                        # required
@@ -10,3 +11,4 @@ if Rails.env.production?
     config.fog_directory  =  ENV["S3_BUCKET"]              # required
   end
 end
+
