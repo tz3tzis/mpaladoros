f78dd566ac8685cf7407bee49b322b32ddf874e8
tz3tzis <mtzetzis@hotmail.com>
2018-12-25 18:00:56 +0100

    merry christmas

--- app/assets/javascripts/serviceworker.js
+++ app/assets/javascripts/serviceworker.js
@@ -1,7 +1,10 @@
 // The serviceworker context can respond to 'push' events and trigger
 // notifications on the registration property
+
+
 self.addEventListener("push", function(event) {
-  var title = (event.data && event.data.text().split("-")[0]) || "Message recieved";
+  var title = (event.data && event.data.text().split("-")[0]) 
+                || "Message recieved";
   var body; body = event.data.text().split("-")[1];
   var tag = "push-simple-demo-notification-tag";
   var icon = 'my-push-logo-192x192.png';
