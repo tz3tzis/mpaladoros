ce51b27aa06a99a2f5353992799e863068d13343
tz3tzis <mtzetzis@hotmail.com>
2018-12-25 18:16:29 +0100

    merry christmas

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0,20]
 	    user.name = auth.info.name
-      user.avatar = largeimage.to_s
+      user.avatar = "http://graph.facebook.com/#{auth.uid}/picture?type=normal"
       user.save!
   	end  	
   end
