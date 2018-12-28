cc6ac4fd8182dec89a76c986acdb5ece30676e35
tz3tzis <mtzetzis@hotmail.com>
2018-12-28 15:06:01 +0100

    merry christmas and a happy new year

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,16 +5,20 @@ include Facebook::Messenger
 #Facebook::Messenger::Subscriptions.subscribe(access_token:ENV["ACCESS_TOKEN"])  
 
 Bot.on :message do |message|
-  message.reply(text: 'Καλωσήρθες παιχταρά μου!')
+  message.reply(text: 'Καλωσήρθες στο mpaladoros.gr! Αυτή την στιγμή μιλά με το chatbot της εφαρμογής.
+  	Οι επιλογές σου είναι οι εξής (1) Μπές στον ιστότοπο της εφαρμογής (2) Δές τα τρέχοντα παιχνίδια που είναι ενεργά
+  	(3) Προσκάλεσαι έναν φίλο σου')
+  message.reply(text: ' Για να συνεχίσεις πάτα τον αριθμό που επιθυμείς απο το παρακάτω μενού.')
   message.reply(
 	  attachment: {
 	    type: 'template',
 	    payload: {
 	      template_type: 'button',
-	      text: 'Σου αρέσει η εφαρμογη ?',
+	      text: 'Δίάλεξε υπηρεσία ',
 	      buttons: [
-	        { type: 'postback', title: 'Ναι', payload: 'yes' },
-	        { type: 'postback', title: 'Οχι', payload: 'no' }
+	        { type: 'postback', title: '(1)', payload: '1' },
+	        { type: 'postback', title: '(2)', payload: '2' },
+	        { type: 'postback', title: '(3)', payload: '3' }
 	      ]
 	    }
 	  }
@@ -27,18 +31,14 @@ Bot.on :postback do |postback|
 	postback.sent_at
 	postback.payload
 
-	if postback.payload == "yes"
-		postback.reply(text: "Τότε μπες εδω https://mpaladoros-app.herokuapp.com και ξεκίνα το παιχνίδι! ")
-	elsif postback.payload == 'no'
-		postback.reply(text: "ΑΝΤΕ ΓΕΙΑ!") 
-		postback.reply(
-			attachment: {
-				type: 'image',
-				payload: {
-					url: 'https://www.reader.gr/sites/default/files/styles/reader_l/public/tsoykalas.jpg?itok=WTYkINB5'
-				}
-			}
-		)
+	if postback.payload == "1"
+		postback.reply(text: "Μπες εδω https://mpaladoros-app.herokuapp.com και ξεκίνα το παιχνίδι! ")
+	elsif postback.payload == '2'
+		postback.reply(text: "Μπες εδω https://mpaladoros-app.herokuapp.com/games και ξεκίνα το παιχνίδι! ")
+	elsif postback.payload =='3'
+		postback.reply(text:'το τρία για για')
+	else
+		postback.reply(text:'Μη εγκυρή επιλογή. Ξαναπροσπάθησε')
 	end
 
 end
