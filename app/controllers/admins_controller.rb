4bb4545f6c7aef40e7a6fccb187512d43186eaa7
tz3tzis <mtzetzis@hotmail.com>
2019-01-14 13:12:14 +0200

    ability.rb fixed

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,14 +7,14 @@ class AdminsController < ApplicationController
 
  def edit
 		@admin = Admin.find(params[:id])
-		authorize! :modify, @admin
+		authorize! :update, @admin
  end
 
 
  def stadium_attributes
  		@admin = Admin.find(params[:id])
  		@stadia = Stadium.where("admin_id = #{@admin.id} ")
- 		authorize! :modify, @admin
+ 		authorize! :update, @admin
  end
 
 	def update
